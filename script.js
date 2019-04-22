15bbfa2c50aacbbb38bb1b5386ae3b722d48eea6
Eric Santos <ericsantosbr2@gmail.com>
2019-04-22 19:17:38 -0300

    Updated lol

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
+var parametrosBaixo = {
+	cerveja: 4,
+}
+
+var parametrosMedios = {
+	cerveja: 6,
+}
+
+var parametrosAlto = {
+	cerveja: 10,
+}
+
 function update(){
-	console.log("lel");
 	var tipo, pessoas, kg;
+	
+	tipo = document.getElementsByName("tipoDeConsumo");
+	pessoas = document.getElementById("pessoas");
+	
+	if(document.getElementById("tipo1").checked){
+		console.log("Tipo 1 meuamigo");
+	
+	}
+	else if(document.getElementById("tipo2").checked){
+		console.log("Tipo 2 meuamigo");
+	}
+	
+	else if(document.getElementById("tipo3").checked){
+		console.log("Tipo 3 meuamigo");
+	}
+	else{
+		console.log("Algo deu marginalmente errado")
+	}
 
 }
